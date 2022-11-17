4079145cc5cd48d6ff08d5e73dc64b3ff401e2ba
QuayJ <sjw.wilson@icloud.com>
2022-11-17 13:22:07 -0500

    working on moving footer, working on subscribe box, forms

--- JS/papi.js
+++ JS/papi.js
@@ -32,29 +32,7 @@ setInterval(()=> {
     }
 
     if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) { 
-        alert("Your Score:" + score.innerText + "\n\nPlay Again?") 10s;
+        // alert("Your Score:" + score.innerText + "\n\nPlay Again?");
         location.reload();
     }
 }, 50);
-
-
-/*Below is for the second game play2*/
-/*Below is for the second game play2*/
-const WORLD_WIDTH = 100
-const WORLD_HEIGHT = 30
-const worldElem = document.querySelector('[data-world]')
-
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
-
-function setPixelToWorldScale() {
-    let worldToPixelScale
-    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT)
-    {
-        worldToPixelScale = window.innerWidth / WORLD_WIDTH
-    } else {
-        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
-    }
-    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
-}
